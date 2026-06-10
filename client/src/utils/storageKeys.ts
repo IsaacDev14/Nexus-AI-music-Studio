47cb9d84535ddd4a612635fd3f160333f10ad299
Mwiti Isaac <mwitiisaac14@gmail.com>
2026-06-10 18:51:15 +0300

    Centralize localStorage key constants.

---
+++ client/src/utils/storageKeys.ts
@@ -0,0 +1,10 @@
+export const STORAGE_KEYS = {
+  PRACTICE_LOGS: 'music_studio_logs',
+  TUNER_REF_PITCH: 'tuner_ref_pitch',
+  METRONOME_BPM: 'metronome_bpm',
+  METRONOME_TIME_SIG: 'metronome_time_sig',
+  JAM_HISTORY: 'jam_session_history',
+  USER_INSTRUMENT: 'user_instrument',
+  THEME_MODE: 'theme_mode',
+  WORKFLOW_DATA: 'workflow_builder_data',
+} as const;
