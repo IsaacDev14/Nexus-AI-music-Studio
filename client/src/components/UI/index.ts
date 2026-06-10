2300eff2bbdd066f3c8c766d1493965e128a5d9c
Mwiti Isaac <mwitiisaac14@gmail.com>
2026-06-10 18:53:15 +0300

    Export StatusDot from UI barrel file.

--- client/src/components/UI/index.ts
+++ client/src/components/UI/index.ts
@@ -1,3 +1,4 @@
 export { default as ErrorBanner } from "./ErrorBanner";
 export { default as LoadingState } from "./LoadingState";
 export { default as EmptyState } from "./EmptyState";
+export { default as StatusDot } from "./StatusDot";
