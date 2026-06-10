326df79eb91f5202a0bde8242c3bfc74a20c9cc9
Mwiti Isaac <mwitiisaac14@gmail.com>
2026-06-10 18:53:15 +0300

    Export PageHeader from UI barrel file.

--- client/src/components/UI/index.ts
+++ client/src/components/UI/index.ts
@@ -2,3 +2,4 @@ export { default as ErrorBanner } from "./ErrorBanner";
 export { default as LoadingState } from "./LoadingState";
 export { default as EmptyState } from "./EmptyState";
 export { default as StatusDot } from "./StatusDot";
+export { default as PageHeader } from "./PageHeader";
