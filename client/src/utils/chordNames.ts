5b820636bec194ca05dcfffffa0afa39907166de
Mwiti Isaac <mwitiisaac14@gmail.com>
2026-06-10 18:52:02 +0300

    Add chord name validation and common chord lists.

---
+++ client/src/utils/chordNames.ts
@@ -0,0 +1,9 @@
+export const COMMON_MAJOR_CHORDS = ['C', 'D', 'E', 'F', 'G', 'A', 'B'] as const;
+
+export const COMMON_MINOR_CHORDS = ['Am', 'Bm', 'Cm', 'Dm', 'Em', 'Fm', 'Gm'] as const;
+
+export const COMMON_SEVENTH_CHORDS = ['C7', 'D7', 'E7', 'F7', 'G7', 'A7', 'B7'] as const;
+
+export function isValidChordName(chord: string): boolean {
+  return /^[A-G][#b]?(m|maj|dim|aug|sus|add)?(7|9|11|13|6)?(\/[A-G][#b]?)?$/.test(chord);
+}
